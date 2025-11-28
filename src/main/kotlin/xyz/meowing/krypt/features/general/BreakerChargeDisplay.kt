701179f68db8818f87d70c4ccc661964d7bfc091
aurielyn <aurielyn.dev@gmail.com>
2025-11-28 16:44:25 +0530

    Feat: custom block overlay depending on breaker charges

--- src/main/kotlin/xyz/meowing/krypt/features/general/BreakerChargeDisplay.kt
+++ src/main/kotlin/xyz/meowing/krypt/features/general/BreakerChargeDisplay.kt
@@ -1,8 +1,13 @@
 package xyz.meowing.krypt.features.general
 
+import net.minecraft.client.renderer.RenderType
+import net.minecraft.client.renderer.ShapeRenderer
 import net.minecraft.network.protocol.game.ClientboundContainerSetSlotPacket
+import net.minecraft.world.level.EmptyBlockGetter
+import net.minecraft.world.phys.shapes.CollisionContext
 import tech.thatgravyboat.skyblockapi.api.datatype.DataTypes
 import tech.thatgravyboat.skyblockapi.api.datatype.getData
+import xyz.meowing.knit.api.KnitClient.client
 import xyz.meowing.krypt.annotations.Module
 import xyz.meowing.krypt.api.location.SkyBlockIsland
 import xyz.meowing.krypt.config.ConfigDelegate
@@ -10,11 +15,13 @@ import xyz.meowing.krypt.config.ui.elements.base.ElementType
 import xyz.meowing.krypt.events.core.GuiEvent
 import xyz.meowing.krypt.events.core.LocationEvent
 import xyz.meowing.krypt.events.core.PacketEvent
+import xyz.meowing.krypt.events.core.RenderEvent
 import xyz.meowing.krypt.features.Feature
 import xyz.meowing.krypt.hud.HudManager
 import xyz.meowing.krypt.managers.config.ConfigElement
 import xyz.meowing.krypt.managers.config.ConfigManager
 import xyz.meowing.krypt.utils.rendering.Render2D
+import java.awt.Color
 
 @Module
 object BreakerChargeDisplay : Feature(
@@ -22,9 +29,14 @@ object BreakerChargeDisplay : Feature(
     island = SkyBlockIsland.THE_CATACOMBS
 ) {
     private const val NAME = "Breaker Charge Display"
-    var renderString = ""
+    private var renderString = ""
+    private var charges = 0
 
     private val compact by ConfigDelegate<Boolean>("breakerChargeDisplay.compact")
+    private val renderText by ConfigDelegate<Boolean>("breakerChargeDisplay.renderText")
+    private val outlineBlocks by ConfigDelegate<Boolean>("breakerChargeDisplay.outlineBlocks")
+    private val emptyColor by ConfigDelegate<Color>("breakerChargeDisplay.noChargesColor")
+    private val fullColor by ConfigDelegate<Color>("breakerChargeDisplay.allChargesColor")
 
     override fun addConfig() {
         ConfigManager
@@ -38,16 +50,44 @@ object BreakerChargeDisplay : Feature(
                 )
             )
             .addFeatureOption(
+                "Render text",
+                ConfigElement(
+                    "breakerChargeDisplay.renderText",
+                    ElementType.Switch(false)
+                )
+            )
+            .addFeatureOption(
                 "Compact display",
                 ConfigElement(
                     "breakerChargeDisplay.compact",
                     ElementType.Switch(true)
                 )
             )
+            .addFeatureOption(
+                "Outline blocks",
+                ConfigElement(
+                    "breakerChargeDisplay.outlineBlocks",
+                    ElementType.Switch(true)
+                )
+            )
+            .addFeatureOption(
+                "No charges color",
+                ConfigElement(
+                    "breakerChargeDisplay.noChargesColor",
+                    ElementType.ColorPicker(Color(255, 0, 0, 255))
+                )
+            )
+            .addFeatureOption(
+                "All charges color",
+                ConfigElement(
+                    "breakerChargeDisplay.allChargesColor",
+                    ElementType.ColorPicker(Color(0, 255, 0, 255))
+                )
+            )
     }
 
     override fun initialize() {
-        HudManager.register(NAME, "§c⸕§e20", "breakerChargeDisplay")
+        HudManager.register(NAME, "§c⸕§e20", "breakerChargeDisplay.renderText")
 
         register<PacketEvent.Received> { event ->
             val packet = event.packet as? ClientboundContainerSetSlotPacket ?: return@register
@@ -55,9 +95,11 @@ object BreakerChargeDisplay : Feature(
 
             if (stack.getData(DataTypes.SKYBLOCK_ID)?.skyblockId?.equals("DUNGEONBREAKER") == false) return@register
 
-            val charges = stack.getData(DataTypes.DUNGEONBREAKER_CHARGES) ?: return@register
-            val first = charges.first
-            val max = charges.second
+            val dbCharges = stack.getData(DataTypes.DUNGEONBREAKER_CHARGES) ?: return@register
+            val first = dbCharges.first
+            val max = dbCharges.second
+
+            charges = first
 
             val colorCode = when {
                 first >= 15 -> "§a"
@@ -69,6 +111,7 @@ object BreakerChargeDisplay : Feature(
         }
 
         register<GuiEvent.Render.HUD> { event ->
+            if (!renderText) return@register
             if (renderString.isEmpty()) return@register
 
             val x = HudManager.getX(NAME)
@@ -78,8 +121,40 @@ object BreakerChargeDisplay : Feature(
             Render2D.renderString(event.context, renderString, x, y, scale)
         }
 
+        register<RenderEvent.World.BlockOutline> { event ->
+            if (!outlineBlocks) return@register
+
+            val blockPos = event.context.blockPos() ?: return@register
+            val blockState = event.context.blockState() ?: return@register
+            val matrixStack = event.context.matrixStack() ?: return@register
+            val consumers = event.context.consumers()
+            val camera = client.gameRenderer.mainCamera
+            val blockShape = blockState.getShape(
+                EmptyBlockGetter.INSTANCE,
+                blockPos,
+                CollisionContext.of(camera.entity)
+            )
+            if (blockShape.isEmpty) return@register
+
+            val camPos = camera.position
+            event.cancel()
+
+            val color = if (charges == 0) emptyColor else fullColor
+
+            ShapeRenderer.renderShape(
+                matrixStack,
+                consumers.getBuffer(RenderType.lines()),
+                blockShape,
+                blockPos.x - camPos.x,
+                blockPos.y - camPos.y,
+                blockPos.z - camPos.z,
+                color.rgb
+            )
+        }
+
         register<LocationEvent.WorldChange> {
             renderString = ""
+            charges = 0
         }
     }
 }
